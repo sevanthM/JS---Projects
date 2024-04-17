44290b702cd4d5a34cc4bcfda1d7f84c539b8bfa
SEVANTH .M. <93463872+sevanthM@users.noreply.github.com>
2024-04-17 15:38:53 +0000

    datatypes and variables

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";
+let age = 23
+let hisName ="Sevanth"
+let isHeComing = false 
+let temp = null 
+let hotSummer
+
+console.log(typeof age);
+console.log(typeof hisName);
+console.log(typeof temp); 
+console.log(typeof isHeComing);
+console.log(typeof hotSummer)
+
+
+
+
+
+
+//DataTypes are
+// number
+// bigInt
+// string
+// boolean
+// symbol - used in figma, react js
+// undefined
+// null 
+
+
+
+// null is object - type
+// undefined is a special DataType
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const fixednum=1234
+let accountMail="sevanthmallikarjun@gmail.com"
+accountPin = "07741820"
+
+
+console.log(fixednum)
+console.log(accountMail)
+console.log(accountPin)
+console.table({accountMail,accountPin, fixednum})
+
+
+
+
+
+
+
+
+
+// avoid using var. it is outdated in js. out of scope issue- var 
+// use let and const 
+// log displays on console
+// table displays in table format
