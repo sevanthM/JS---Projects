81c559338454a66d3a71fca0f5360da0e4fdc027
SEVANTH .M. <93463872+sevanthM@users.noreply.github.com>
2024-04-18 04:50:20 +0000

    datatype summary , operations and comparisons

---
+++ 01_basics/01_conversionOperations.js
@@ -0,0 +1,54 @@
+let myName = "samsmith"
+let age = 42
+let isHeComing = false 
+let randDataType = null 
+let randDataType2 = undefined
+
+// for dataType conversion use capital for first letter to wished DT change
+
+let newName = Number(myName)
+console.log(newName);
+console.log(typeof newName);
+
+let newAge = String(age)
+console.log(age)
+console.log(typeof age)
+
+let newHeComing = Number(isHeComing)
+console.log(newHeComing);
+
+
+// for dataType conversion use capital for first letter to wished DT change 
+
+/*let number1 = 32
+let String1 = Boolean(number1)
+console.log(String1) */
+
+// basic arithmetic operations 
+console.log(2+2);
+console.log(2*2);
+console.log(2**2); // 2 to the power of 2 
+console.log(2-2);
+console.log(2/2);
+console.log(2%2); // modulas
+
+// + , - , * , / , % , ** 
+
+// dataType comparisons
+console.log(2>=3); 
+console.log(4<=6);
+console.log(4!=4);
+console.log("2"==2)
+
+
+
+
+
+
+
+
+
+// concatenation of DataTypes 
+let myNewNamenow = "ssam"
+let myLastnamenow = "smith"
+console.log(myNewNamenow+myLastnamenow);
---
+++ 01_basics/01_datatypesSummary.js
@@ -0,0 +1,7 @@
+// -------------------------------2 types of dataTypes------------------------------------- 
+// - call by value PRIMITIVE DT 
+// for PRIMITIVE : 7 values 
+// String , Number , Boolean , null , undefined , Symbol , bigInt 
+
+// - call by reference / reference DT - NON PRIMITIVE DT 3 values  
+//  NON PRIMITIVE : 3 values
